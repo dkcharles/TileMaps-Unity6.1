ea3f62759e3520910a89b31730047d6724153966
Darryl Charles <dk.charles@ulster.ac.uk>
2025-10-20 08:29:14 +0100

    slight update

--- Assets/_Game/Code/PlayerController_TopDown.cs
+++ Assets/_Game/Code/PlayerController_TopDown.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 public class PlayerController_TopDown : MonoBehaviour
 {
     [Header("Movement Settings")]
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float moveSpeed;
     
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    void Awake()
+    { 
+
+    }
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
     }
     
     void Update()
